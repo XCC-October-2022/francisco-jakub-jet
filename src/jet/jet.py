de23eec318808faa344240f50129eb59def014d7
GetTuh <jakubniemczyk97@gmail.com>
2022-10-05 15:19:03 +0200

    Added repo listing

--- src/jet/jet.py
+++ src/jet/jet.py
@@ -6,8 +6,10 @@ import os
 load_dotenv()
 
 logger = structlog.getLogger(__name__)
-git_api = Github('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')
+git_api = Github('key')
 
 
 def list_repos(roman_number: str) -> int:
-    ...
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+
